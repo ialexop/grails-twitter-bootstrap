4d1dbd50aa1300c80202955fd0b35679dd37d1c2
Iraklis Alexopoulos <alexopoulosiraklis@gmail.com>
2013-05-25 21:12:39 +0400

    Add required config for the Bootstrap plugin.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -89,3 +89,6 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+grails.plugins.twitterbootstrap.fixtaglib = true
+grails.plugins.twitterbootstrap.defaultBundle = 'bundle_bootstrap'
