199a6d6d9ab78c795055fda19c89b36d38fb15ab
Iraklis Alexopoulos <alexopoulosiraklis@gmail.com>
2013-05-25 20:55:10 +0400

    Replace LESS resources plugin with LESSCSS plugin.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,7 +49,7 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.8.3"
         runtime ":resources:1.1.6"
         compile ":twitter-bootstrap:2.3.0"
-        compile ":less-resources:1.3.0.2"
+        compile ":lesscss-resources:1.3.3"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
